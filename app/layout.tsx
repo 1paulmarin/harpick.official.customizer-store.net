c9c41a7a694d96a8859a59aad74be090d6b4d504
1paulmarin <paulmarin663@gmail.com>
2025-08-03 22:35:19 +0300

    Fix correct path to globals.css in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type React from "react"
 import { Inter } from "next/font/google"
-import "./globals.css"
+import '../globals.css'
+
+
 import { ThemeProvider } from "@/components/theme-provider"
 import { Toaster } from "sonner"
 import { Navigation } from "@/components/navigation"
